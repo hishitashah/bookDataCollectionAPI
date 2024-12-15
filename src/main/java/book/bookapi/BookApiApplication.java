0f22794ed62e3cbad6ca1f41444b9244c0f85f11
hishitashah <hishita.shah@gmail.com>
2024-12-16 00:29:26 +0300

    pom file changed

---
+++ src/main/java/book/bookapi/BookApiApplication.java
@@ -0,0 +1,11 @@
+package book.bookapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BookApiApplication.class, args);
+    }
+}
